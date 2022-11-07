bb6a5fe2faaf1fe1a9b7770d007a670b00f1680c
Alessia B <borys.alessia@gmail.com>
2022-11-07 10:46:29 +0100

    new

--- sketch/circlePacking.ts
+++ sketch/circlePacking.ts
@@ -1,10 +1,10 @@
 interface CircleData {
-    position: Position;
-    radius: number;
+  position: Position;
+  radius: number;
 }
 interface Position {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
 
 /**
@@ -13,29 +13,42 @@ interface Position {
  * @param areaHeight the height of the area within which circles may be generated
  * @return an array of generated Circle data objects
  */
+
+function areOverlapping(c1: CircleData, c2: CircleData): boolean {
+  const distanceBetween = distance(c1.position, c2.position);
+  return c1.radius + c2.radius > distanceBetween;
+}
+
 function calculatePackedCircles(
-    areaWidth: number,
-    areaHeight: number
+  areaWidth: number,
+  areaHeight: number
 ): CircleData[] {
-    //TODO: you need to implement this function properly!
-    //These are just a couple of random circles, with no consideration yet for avoiding overlap.
-    const circleDataOne: CircleData = {
-        position: { x: 300, y: 300 },
-        radius: 100,
-    };
+  //TODO: you need to implement this function properly!
+  //These are just a couple of random circles, with no consideration yet for avoiding overlap.
+  const circleDataOne: CircleData = {
+    position: { x: 300, y: 300 },
+    radius: 200,
+  };
+  const createdCircles = [circleDataOne];
+  for (let circleNo = 0; circleNo < 1000; circleNo++) {
     const circleDataTwo: CircleData = {
-        position: { x: random(0, areaWidth), y: random(0, areaHeight) },
-        radius: 40,
+      position: { x: random(0, areaWidth), y: random(0, areaHeight) },
+      radius: random(0, 200),
     };
-    return [circleDataOne, circleDataTwo];
+    if (areOverlapping(circleDataOne, circleDataTwo) === false) {
+      createdCircles.push(circleDataTwo);
+    }
+  }
+  return createdCircles;
+  //return [circleDataOne, undefined]
 }
 
 /** Returns the distance between two given positions.
     This function doesn't require the p5.js library.
  */
 function distance(p1: Position, p2: Position): number {
-    const x = p1.x - p2.x;
-    const y = p1.y - p2.y;
-    const hyp = Math.sqrt(x * x + y * y);
-    return hyp;
+  const x = p1.x - p2.x;
+  const y = p1.y - p2.y;
+  const hyp = Math.sqrt(x * x + y * y);
+  return hyp;
 }
